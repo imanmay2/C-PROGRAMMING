7209ad6ffac1edcd65f936bded104cfd07576ddd
SoumadeepChoudhury <87203832+SoumadeepChoudhury@users.noreply.github.com>
2021-11-09 23:50:15 +0530

    Basic Pattern updated.

--- pattern1.c
+++ pattern1.c
@@ -1,10 +1,21 @@
+// Print the following pattern 
+/* 
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+*/
+
 #include<stdio.h>
 int main()
 {
     int i,j,n;
-    printf("enter the number of rows you want to print");
+    printf("Enter the number of rows you want to print: ");
     scanf("%d", &n);
-    printf("pattern is as follows....\n");
+    printf("Pattern is as follows....\n");
     for(i=1;i<=n;i++)
     {
         for(j=1;j<=i;j++){
