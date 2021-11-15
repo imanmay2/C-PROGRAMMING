14ec543378ddfb96d3773653bbab029b73d27282
SoumadeepChoudhury <87203832+SoumadeepChoudhury@users.noreply.github.com>
2021-11-16 01:19:06 +0530

    Corrected Pattern

--- pattern2.c
+++ pattern2.c
@@ -1,30 +1,30 @@
 // print the following pattern.....
-      *
+/*    *
       *
 *  *  *  *  *
       *
-      * //
+      * 
+      
+*/
 #include<stdio.h>
 int main()
 {
     int i,j;
     printf("pattern is as follows\n");
-    for(i=1;i<=5;i++)
-    {
-        for (j = 0; i <=5; j++)
-        {
-            if(j==3 || i==3)
-            {
-                printf("* ");
-            }
+    for (i =0; i<5;i++){
+        if(i!=2){
+        for (j=0;j<5;j++){
+            if(j==3)
+                printf("*");
             else
-            {
-                printf(" ");
-            }
-        printf("\n");
+                printf("  ");
+        }}
+        else{
+            for (j=0;j<5;j++){
+                printf("*  ");
         }
-        
-        
+        }
+        printf("\n");
     }
     return 0;
 }
