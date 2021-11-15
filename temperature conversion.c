3756feeaddd023b5a799a986977a86dbc2c1bff0
SoumadeepChoudhury <87203832+SoumadeepChoudhury@users.noreply.github.com>
2021-11-16 01:11:55 +0530

    Corrected Program

--- temperature conversion.c
+++ temperature conversion.c
@@ -5,7 +5,7 @@ int main()
     printf("WELCOME TO THE TEMPEARTURE CONVERTER");
     printf("Enter the temperature in Celcius degree..");
     scanf("%d",&c);
-    f=((9*c)/32)+32;
+    f=((9*c)/5)+32;
     printf("%s,%d","temperature in  Farenheit is",f);
     k=c+273;
     printf("%s,%d","temperature in  kelvin is",k);
