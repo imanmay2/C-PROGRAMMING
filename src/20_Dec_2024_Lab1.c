a0b276583218384e13c223bddbda6fe2e861ef1d
manmay2 <imanmay2@gmail.com>
2024-12-23 15:25:49 +0530

    updated

--- src/20_Dec_2024_Lab1.c
+++ src/20_Dec_2024_Lab1.c
@@ -17,6 +17,7 @@
 
 
 
+
 // // Write a program for checking whether the accepted number is positive,negative or 0.
 // #include<stdio.h>
 // int main(){
@@ -34,6 +35,7 @@
 //     }
 //     return 0;
 // }
+
 
 
 
@@ -57,36 +59,59 @@
 
 
 
-//Write a program for printing the week in a day using SWITCH STATEMENT.
+
+// //Write a program for printing the week in a day using SWITCH STATEMENT.
+// #include<stdio.h>
+// int main(){
+//     int day;
+//     printf(" Enter the day from 1-7 : ");
+//     scanf("%d",&day);
+//     switch(day){
+//         case 1:
+//         printf("Monday");
+//         break;
+//         case 2:
+//         printf("Tuesday");
+//         break;
+//         case 3:
+//         printf("Wednesday");
+//         break;
+//         case 4:
+//         printf("Thrusday");
+//         break;
+//         case 5:
+//         printf("Friday");
+//         break;
+//         case 6:
+//         printf("Saturday");
+//         break;
+//         case 7:
+//         printf("Sunday");
+//         break;
+//         default:
+//         printf("INVALID NUMBER ENTERED ! ENTER BETWEEN 1-7 . ");
+//         break;
+//     }
+// }
+
+
+
+// Write the program to input a number of 3 digit and print the sum of last and first digit of the number.
 #include<stdio.h>
 int main(){
-    int day;
-    printf(" Enter the day from 1-7 : ");
-    scanf("%d",&day);
-    switch(day){
-        case 1:
-        printf("Monday");
-        break;
-        case 2:
-        printf("Tuesday");
-        break;
-        case 3:
-        printf("Wednesday");
-        break;
-        case 4:
-        printf("Thrusday");
-        break;
-        case 5:
-        printf("Friday");
-        break;
-        case 6:
-        printf("Saturday");
-        break;
-        case 7:
-        printf("Sunday");
-        break;
-        default:
-        printf("INVALID NUMBER ENTERED ! ENTER BETWEEN 1-7 : ");
-        break;
+    int num;
+    printf("Enter the number : ");
+    scanf("%d",&num);
+    int ct=0,sum=0;
+    while(num!=0){
+        int r=num%10;
+        if(ct==0 || ct==2){    
+            sum+=r;
+        }
+        num=(int) num/10;
+        ct++;
     }
+    
+    printf("Sum of 1st and last digit is : %d",sum);
+    return 0;
 }
