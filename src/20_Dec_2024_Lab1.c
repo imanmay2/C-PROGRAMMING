7d7bf1cc54772c7a9ad1fb5b3ab6d33f98679fe7
manmay2 <imanmay2@gmail.com>
2024-12-23 15:45:51 +0530

    Assignment done.

--- src/20_Dec_2024_Lab1.c
+++ src/20_Dec_2024_Lab1.c
@@ -1,98 +1,98 @@
 // // Q1. Write a program to take name,age and check eligiblity for voting.
 
-// #include<stdio.h>
-// int main(){
-//     int age;
-//     char name[100];
-//     printf("Enter the name and age of the user :");
-//     scanf("%s %d",&name,&age);
-//     if(age>=18){
-//         printf("%s is eligible for vote.",name);
-//     }
-//     else{
-//         printf("%s is not eligible for vote",name);
-//     }
-//     return 0;
-// }
+#include<stdio.h>
+int main(){
+    int age;
+    char name[100];
+    printf("Enter the name and age of the user :");
+    scanf("%s %d",&name,&age);
+    if(age>=18){
+        printf("%s is eligible for vote.",name);
+    }
+    else{
+        printf("%s is not eligible for vote",name);
+    }
+    return 0;
+}
 
 
 
 
 // // Write a program for checking whether the accepted number is positive,negative or 0.
-// #include<stdio.h>
-// int main(){
-//     int num;
-//     printf("Enter the number : ");
-//     scanf("%d",&num);
-//     if(num>0){
-//         printf("%d is Positive.",num);
-//     }
-//     else if(num<0){
-//         printf("%d is Negative.",num);
-//     }
-//     else{
-//         printf("%d is Zero.",num);
-//     }
-//     return 0;
-// }
+#include<stdio.h>
+int main(){
+    int num;
+    printf("Enter the number : ");
+    scanf("%d",&num);
+    if(num>0){
+        printf("%d is Positive.",num);
+    }
+    else if(num<0){
+        printf("%d is Negative.",num);
+    }
+    else{
+        printf("%d is Zero.",num);
+    }
+    return 0;
+}
 
 
 
 
 // Write a program for find the maximum of 3 numbers using nested-if.
-// #include<stdio.h>
-// int main(){
-//     int num1,num2,num3;
-//     printf("Enter the 3 numbers : ");
-//     scanf("%d %d %d",&num1,&num2,&num3);
-//     if(num1>num2){
-//         if(num1>num3){
-//             printf("%d is the maximum one.",num1);
-//         }
-//     } else if(num2>num3){
-//         printf("%d is the maximum one.",num2);
-//     }
-//     else{
-//         printf("%d is the maximum one. ",num3);
-//     }
-// }
+#include<stdio.h>
+int main(){
+    int num1,num2,num3;
+    printf("Enter the 3 numbers : ");
+    scanf("%d %d %d",&num1,&num2,&num3);
+    if(num1>num2){
+        if(num1>num3){
+            printf("%d is the maximum one.",num1);
+        }
+    } else if(num2>num3){
+        printf("%d is the maximum one.",num2);
+    }
+    else{
+        printf("%d is the maximum one. ",num3);
+    }
+}
 
 
 
 
 // //Write a program for printing the week in a day using SWITCH STATEMENT.
-// #include<stdio.h>
-// int main(){
-//     int day;
-//     printf(" Enter the day from 1-7 : ");
-//     scanf("%d",&day);
-//     switch(day){
-//         case 1:
-//         printf("Monday");
-//         break;
-//         case 2:
-//         printf("Tuesday");
-//         break;
-//         case 3:
-//         printf("Wednesday");
-//         break;
-//         case 4:
-//         printf("Thrusday");
-//         break;
-//         case 5:
-//         printf("Friday");
-//         break;
-//         case 6:
-//         printf("Saturday");
-//         break;
-//         case 7:
-//         printf("Sunday");
-//         break;
-//         default:
-//         printf("INVALID NUMBER ENTERED ! ENTER BETWEEN 1-7 . ");
-//         break;
-//     }
-// }
+#include<stdio.h>
+int main(){
+    int day;
+    printf(" Enter the day from 1-7 : ");
+    scanf("%d",&day);
+    switch(day){
+        case 1:
+        printf("Monday");
+        break;
+        case 2:
+        printf("Tuesday");
+        break;
+        case 3:
+        printf("Wednesday");
+        break;
+        case 4:
+        printf("Thrusday");
+        break;
+        case 5:
+        printf("Friday");
+        break;
+        case 6:
+        printf("Saturday");
+        break;
+        case 7:
+        printf("Sunday");
+        break;
+        default:
+        printf("INVALID NUMBER ENTERED ! ENTER BETWEEN 1-7 . ");
+        break;
+    }
+}
 
 
 
@@ -114,4 +114,33 @@ int main(){
     
     printf("Sum of 1st and last digit is : %d",sum);
     return 0;
+}
+
+
+// Write a program to calculate the grade of 5 subjects.
+#include<stdio.h>
+int main(){
+    int m1,m2,m3,m4,m5;
+    printf("Enter the Marks of 5 subjects : ");
+    scanf("%d %d %d %d %d",&m1,&m2,&m3,&m4,&m5);
+    int avg=(m1+m2+m3+m4+m5)/5;
+    if(avg>=90){
+        printf("Grade S");
+    }
+    else if(avg>=80){
+        printf("Grade A");
+    }
+
+    else if(avg>=70){
+        printf("Grade B");
+    }
+
+    else if(avg>=60){
+        printf("Grade C");
+    }
+    else if(avg>=50){
+        printf("Grade D");
+    } else{
+        printf("Grade F");
+    }
 }
