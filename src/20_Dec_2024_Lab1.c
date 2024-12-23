71e39630c70b009b0516fc21db1c30c61d131ead
manmay2 <imanmay2@gmail.com>
2024-12-23 12:03:26 +0530

    Q4 done using switch statement.

--- src/20_Dec_2024_Lab1.c
+++ src/20_Dec_2024_Lab1.c
@@ -38,19 +38,55 @@
 
 
 // Write a program for find the maximum of 3 numbers using nested-if.
+// #include<stdio.h>
+// int main(){
+//     int num1,num2,num3;
+//     printf("Enter the 3 numbers : ");
+//     scanf("%d %d %d",&num1,&num2,&num3);
+//     if(num1>num2){
+//         if(num1>num3){
+//             printf("%d is the maximum one.",num1);
+//         }
+//     } else if(num2>num3){
+//         printf("%d is the maximum one.",num2);
+//     }
+//     else{
+//         printf("%d is the maximum one. ",num3);
+//     }
+// }
+
+
+
+//Write a program for printing the week in a day using SWITCH STATEMENT.
 #include<stdio.h>
 int main(){
-    int num1,num2,num3;
-    printf("Enter the 3 numbers : ");
-    scanf("%d %d %d",&num1,&num2,&num3);
-    if(num1>num2){
-        if(num1>num3){
-            printf("%d is the maximum one.",num1);
-        }
-    } else if(num2>num3){
-        printf("%d is the maximum one.",num2);
-    }
-    else{
-        printf("%d is the maximum one. ",num3);
+    int day;
+    printf(" Enter the day from 1-7 : ");
+    scanf("%d",&day);
+    switch(day){
+        case 1:
+        printf("Monday");
+        break;
+        case 2:
+        printf("Tuesday");
+        break;
+        case 3:
+        printf("Wednesday");
+        break;
+        case 4:
+        printf("Thrusday");
+        break;
+        case 5:
+        printf("Friday");
+        break;
+        case 6:
+        printf("Saturday");
+        break;
+        case 7:
+        printf("Sunday");
+        break;
+        default:
+        printf("INVALID NUMBER ENTERED ! ENTER BETWEEN 1-7 : ");
+        break;
     }
 }
