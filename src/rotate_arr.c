bbe85602e4e81e1e96d831cc8820e105b2275123
manmay2 <imanmay2@gmail.com>
2025-01-05 11:27:30 +0530

    yet to be done

---
+++ src/rotate_arr.c
@@ -0,0 +1,15 @@
+// Rotate array ---
+// [1,2,3,4,5,6,7,8]----> n=6 ---> output --> [3,4,5,6,7,8,1,2]
+
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the length of the array : ");
+    scanf("%d",&n);
+    int arr[n];
+
+    int r;
+    printf("Enter the number to be rotated : ");
+    
+}
