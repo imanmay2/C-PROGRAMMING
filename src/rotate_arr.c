4cadb238a333330152b4b1370a25ec4c541cc614
manmay2 <imanmay2@gmail.com>
2025-01-17 21:40:34 +0530

    updated

--- src/rotate_arr.c
+++ src/rotate_arr.c
@@ -1,15 +1,31 @@
-// Rotate array ---
-// [1,2,3,4,5,6,7,8]----> n=6 ---> output --> [3,4,5,6,7,8,1,2]
-
 
 #include<stdio.h>
-int main(){
-    int n;
-    printf("Enter the length of the array : ");
-    scanf("%d",&n);
-    int arr[n];
+#include<ctype.h>
+#include<string.h>
 
-    int r;
-    printf("Enter the number to be rotated : ");
-    
+int main(){
+    char str[100];
+    printf("Enter the String : ");
+    // fgets(str,100,stdin);
+    scanf("%[^\n]%*c",str);
+    puts(str);
+    int arr[]={0,0,0,0};  // Chronological order----> Vowels, Consonants,Digits,Special Character.
+    for(int i=0;i<strlen(str);i++){
+       if(str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U' || str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u'){
+        arr[0]++;
+       } else if(str[i]=='0' || str[i]=='1' || str[i]=='2' || str[i]=='3' || str[i]=='4' || str[i]=='5' || str[i]=='6' || str[i]=='7' || str[i]=='8' || str[i]=='9'){
+        arr[2]++;
+       } else if(isalpha(str[i])!=0){
+        arr[1]++;
+       } else{
+        arr[3]++;
+       }
+    }
+
+
+    printf("Count of Vowels : %d\n",arr[0]);
+    printf("Count of Consonants: %d\n",arr[1]);
+    printf("Count of Digits: %d\n",arr[2]);
+    printf("Count of Special Characters: %d\n",arr[3]);
+    return 0;
 }
