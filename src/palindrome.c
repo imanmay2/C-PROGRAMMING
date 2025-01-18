4168f77482345e2a2f10b03919c3d4b787177076
manmay2 <imanmay2@gmail.com>
2025-01-18 21:39:07 +0530

    palindrome working succesfully.

--- src/palindrome.c
+++ src/palindrome.c
@@ -3,11 +3,23 @@
 
 #include<stdio.h>
 #include<string.h>
+int isPalindrome(char *str){
+    int left=0;
+    int right=strlen(str)-1;
+    while(left<right){
+        if(str[left]!=str[right]){
+            return 0;
+        }
+        left++;
+        right--;
+    }
+    return 1;
+}
+
 
 int main(){
-    char str[100];
-    printf("Enter the String : ");
-    scanf("%[^\n]%*c",str);
-    puts(str);
-
+    char str[50];
+    scanf("%s",str);
+    printf("String inputted is: %s",str);
+    printf("%d",isPalindrome(str));
 }
