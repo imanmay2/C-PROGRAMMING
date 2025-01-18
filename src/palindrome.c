aa83f9d57e837bf24d58c15afecb9f2e24825406
manmay2 <imanmay2@gmail.com>
2025-01-18 22:21:54 +0530

    bug to be fixed

--- src/palindrome.c
+++ src/palindrome.c
@@ -16,10 +16,61 @@ int isPalindrome(char *str){
     return 1;
 }
 
+void reverseString(char *str){
+    int left=0;
+    int right=strlen(str)-1;
+    char temp;
+    while(left<right){
+        temp=str[left];
+        str[left]=str[right];
+        str[right]=str[left];
+    }
+}
+
+
 
 int main(){
     char str[50];
     scanf("%s",str);
-    printf("String inputted is: %s",str);
-    printf("%d",isPalindrome(str));
+    // printf("String inputted is: %s",str);
+    // printf("%d",isPalindrome(str));
+
+    int left=0;
+    int right=strlen(str)-1;
+    int maxOri=0;
+    int maxRev=0;
+    char ss[strlen(str)];
+    while(left<right){
+        strncpy(ss,str+left,right);
+        if(isPalindrome(ss)){
+            int count=strlen(ss);
+            if(count>maxOri){
+                maxOri=count;
+            }
+        }
+        left++;
+    }
+
+
+    //copy and then reversing the string.
+    char str2[strlen(str)];
+    strcpy(str2,str);
+    reverseString(str2);
+    left=0;
+    right=strlen(str2)-1;
+    while(left<right){
+        strncpy(ss,str+left,right);
+        if(isPalindrome(ss)){
+            int count=strlen(ss);
+            if(count>maxRev){
+                maxRev=count;
+            }
+        }
+        left++;
+    }
+
+
+    printf("%d",(maxRev>maxOri)?maxRev:maxOri);
+
+
 }
