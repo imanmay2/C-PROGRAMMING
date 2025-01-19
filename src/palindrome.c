73f9bfbc3ca605af63d6cc63f01562f43d0abdc3
manmay2 <imanmay2@gmail.com>
2025-01-19 11:29:04 +0530

    bug not to be fixed

--- src/palindrome.c
+++ src/palindrome.c
@@ -1,20 +1,13 @@
 //check whether the string is palindrome or not.
 
+//banana ---> 
+//acacacab -->
+
+
 
 #include<stdio.h>
 #include<string.h>
-int isPalindrome(char *str){
-    int left=0;
-    int right=strlen(str)-1;
-    while(left<right){
-        if(str[left]!=str[right]){
-            return 0;
-        }
-        left++;
-        right--;
-    }
-    return 1;
-}
+
 
 void reverseString(char *str){
     int left=0;
@@ -23,15 +16,45 @@ void reverseString(char *str){
     while(left<right){
         temp=str[left];
         str[left]=str[right];
-        str[right]=str[left];
+        str[right]=temp;
+        left++;
+        right--;
     }
+}
+
+
+int isPalindrome(char *str){
+    int left=0;
+    int right=strlen(str)-1;
+    char revStr[strlen(str)];
+    strcpy(revStr,str);
+    reverseString(revStr);
+    int i=0;
+    while(1)
+    {
+        printf("%s -> %s \n",str,revStr);
+        printf("%c -> %c \n",*(str+i),*(revStr+i));
+        // if(*(str+i) != *(revStr+i)){
+        //     return 0;
+        // }
+        i++;
+        if(i>=strlen(str)){
+            return 1;
+        }
+    }
+    return 0;
 }
 
 
 
 int main(){
-    char str[50];
-    scanf("%s",str);
+    char str[]="banana\n";
+    // String str="banana"; //best 
+
+    // scanf("%[^\n]%*c",str);
+    // fgets(str,50,stdin);
+    // printf("%s",str);
+    
     // printf("String inputted is: %s",str);
     // printf("%d",isPalindrome(str));
 
@@ -40,8 +63,11 @@ int main(){
     int maxOri=0;
     int maxRev=0;
     char ss[strlen(str)];
-    while(left<right){
+
+    while(left<right-1){
         strncpy(ss,str+left,right);
+        // printf("%s",ss);
+        // printf("%d",isPalindrome(ss));
         if(isPalindrome(ss)){
             int count=strlen(ss);
             if(count>maxOri){
@@ -49,27 +75,30 @@ int main(){
             }
         }
         left++;
+
     }
 
 
-    //copy and then reversing the string.
-    char str2[strlen(str)];
-    strcpy(str2,str);
-    reverseString(str2);
-    left=0;
-    right=strlen(str2)-1;
-    while(left<right){
-        strncpy(ss,str+left,right);
-        if(isPalindrome(ss)){
-            int count=strlen(ss);
-            if(count>maxRev){
-                maxRev=count;
-            }
-        }
-        left++;
-    }
+
+    // //copy and then reversing the string.
+    // char str2[strlen(str)];
+    // strcpy(str2,str);
+    // reverseString(str2);
+    // left=0;
+    // right=strlen(str2)-1;
+    // while(left<right){
+    //     strncpy(ss,str+left,right);
+    //     if(isPalindrome(ss)){
+    //         int count=strlen(ss);
+    //         if(count>maxRev){
+    //             maxRev=count;
+    //         }
+    //     }
+    //     left++;
+    // }
 
 
+    // printf("%d %d\n",maxOri,maxRev);
     printf("%d",(maxRev>maxOri)?maxRev:maxOri);
 
 
