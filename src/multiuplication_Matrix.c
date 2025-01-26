02217bcebbda81f0516ab2115c8c097d4a89b0a5
manmay2 <imanmay2@gmail.com>
2025-01-26 11:12:48 +0530

    multiplication matrix yet to be solved

---
+++ src/multiuplication_Matrix.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d",&n);
+    int arr1[n][n];
+    int arr2[n][n];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("Enter the element for Array1: ");
+            scanf("%d",&arr1[i][j]);
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("Enter the element for Array2: ");
+            scanf("%d",&arr2[i][j]);
+        }
+    }
+
+
+    //main algo starts from here.
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            
+        }
+    }
+
+}
