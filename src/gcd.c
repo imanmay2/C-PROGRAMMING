950a007b0cf683824da7d770b48d463dba1870cc
manmay2 <imanmay2@gmail.com>
2025-03-05 09:09:52 +0530

    GCD of the numbers is updated

---
+++ src/gcd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n1=4,n2=2;
+    int gcd=1;
+    for(int i=1;i<=n1 && i<=n2;i++){
+        if(n1%i==0 && n2%i==0){
+            gcd=i;
+        }
+    }
+
+
+    printf("GCD of the numbers is : %d",gcd);
+}
