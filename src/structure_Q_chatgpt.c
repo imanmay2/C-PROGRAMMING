dee8c42f025f3b59cc1551228a20a46b5a16aefa
manmay2 <imanmay2@gmail.com>
2025-03-16 19:12:42 +0530

    Structure Question Done (Chatgpt Question).

---
+++ src/structure_Q_chatgpt.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+struct student{
+    char name[100];
+    int roll;
+    double marks[3];
+    double TotalMarks;
+};
+
+void display(struct student *arr,int n){
+    for(int i=0;i<n;i++){
+        double totalMarks=0;
+        printf("Student's Name: %s Roll: %d Total Marks: %.2lf\n",arr[i].name,arr[i].roll,arr[i].TotalMarks);
+    }
+}
+
+
+int main(){
+    int n;
+    printf("Enter number of Students : ");
+    scanf("%d",&n);
+
+    struct student arr[n];
+    for(int i=0;i<n;i++){
+        printf("Enter name : ");
+        scanf("%s",arr[i].name);
+        printf("Enter roll : ");
+        scanf("%d",&arr[i].roll);
+        int size=sizeof(arr[i].marks)/sizeof(arr[i].marks[0]);
+        int sum=0;
+        for(int j=0;j<size;j++){
+            printf("Enter Marks %d : ",j+1);
+            scanf("%lf",&arr[i].marks[j]);
+            sum=sum+arr[i].marks[j];
+        }
+        arr[i].TotalMarks=sum;
+    }
+    
+    display(arr,n);
+
+}
