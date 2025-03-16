643bc3fb8977a3aa34ac03d74323db614ec9789d
manmay2 <imanmay2@gmail.com>
2025-03-16 21:59:01 +0530

    transpose using pointer arithmetic

---
+++ src/pointer_testing.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void print2DArray(int *arr,int row,int col){
+    for(int i=0;i<col;i++){
+        for(int j=0;j<row;j++){
+            printf("%d ",*(arr+col*j+i));
+        }
+        printf("\n");
+    }
+}
+
+
+int main(){
+    //print a 2D array using pointer arithmetic.
+
+    int row=3,col=3;
+    int arr[row][col];
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+
+    print2DArray((int *)arr,row,col);
+}
