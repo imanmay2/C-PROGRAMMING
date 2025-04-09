76bca96b19a3ae6b45d8f160ef4ee5f4c532909a
manmay2 <imanmay2@gmail.com>
2025-04-09 08:48:04 +0530

    updated

--- src/reverse_String.c
+++ src/reverse_String.c
@@ -1,23 +1,51 @@
+// #include<stdio.h>
+// void reverseString(char *str){
+//     // printf("%s",str);
+//     char *start=str;
+//     char *end=str;
+//     while(*end){
+//         end++;
+//     }
+//     end--;
+//     while(start<end){
+//         char temp=*start;
+//         *start=*end;
+//         *end=temp;
+//         start++;
+//         end--;
+//     }
+// }
+// int main(){
+//     char str[100];
+//     fgets(str,100,stdin);
+//     reverseString(str);
+//     printf("%s",str);
+// }
+
 #include<stdio.h>
-void reverseString(char *str){
-    // printf("%s",str);
+
+void reverseStr(char *str){
     char *start=str;
     char *end=str;
     while(*end){
         end++;
-    }
-    end--;
+    } end--;
+
     while(start<end){
+        
         char temp=*start;
         *start=*end;
         *end=temp;
+
         start++;
         end--;
     }
 }
+
 int main(){
     char str[100];
     fgets(str,100,stdin);
-    reverseString(str);
+
+    reverseStr(str);
     printf("%s",str);
 }
