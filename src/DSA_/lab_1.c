2f71dab11fd0939a15c7a4c34d8dfe1e052dc386
manmay2 <imanmay2@gmail.com>
2025-07-12 19:11:25 +0530

    Largest and Smallest array

--- src/DSA_/lab_1.c
+++ src/DSA_/lab_1.c
@@ -66,3 +66,38 @@
 
 
 
+//Q4.Find the largest and smallest in an array.
+#include<stdio.h>
+int findLargest(int *arr,int n){
+    int max=arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    return max;
+}
+
+int findSmallest(int *arr,int n){
+    int min=arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i]<min){
+            min=arr[i];
+        }
+    }
+    return min;
+}
+int main(){
+    int n;
+    printf("Enter the range of the array : ");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        printf("Enter the element : ");
+        scanf("%d",&arr[i]);
+    }
+    
+    printf("Smallest Number in the array is : %d\n",findSmallest(arr,n));
+    printf("Largest Number in the array is : %d",findLargest(arr,n));
+
+}
