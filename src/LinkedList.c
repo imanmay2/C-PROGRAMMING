8ea6d2e32a562badd3d1a8dfe6d637aac8eae9dc
manmay2 <imanmay2@gmail.com>
2025-10-04 15:01:43 +0530

    LinkedList started

--- src/LinkedList.c
+++ src/LinkedList.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//creating the struct for the Node.
+
 struct Node{
     int data;
     struct Node* next;
@@ -10,19 +12,34 @@ struct Node* createNode(int value){
     struct Node* newNode=(struct Node*)malloc(sizeof(struct Node));
     newNode->data=value;
     newNode->next=NULL;
+
     return newNode;
 }
 
 void addFirst(struct Node** head,int value){
     struct Node* newNode=createNode(value);
+    if(*head==NULL){
+        *head=newNode;
+        return;
+    }
 
     newNode->next=*head;
     *head=newNode;
+
 }
 
 
-void addLast(struct Node** head,int value){
+void display_LL(struct Node** head){
+    struct Node* temp=*head;
 
+    printf("The LinkedList is as follows : \n");
+    while(temp!=NULL){
+        printf("%d ",temp->data);
+        temp=temp->next;
+    }
+}
+
+void addLast(struct Node** head,int value){
     struct Node* newNode=createNode(value);
     if(*head==NULL){
         *head=newNode;
@@ -33,27 +50,18 @@ void addLast(struct Node** head,int value){
     while(temp->next!=NULL){
         temp=temp->next;
     }
+
     temp->next=newNode;
 }
 
-void display(struct Node** head){
-    struct Node* temp=*head;
 
-    while(temp!=NULL){
-        printf("%d ->",temp->data);
-        temp=temp->next;
-    }
-    printf("->NULL");
-}
 
 
 int main(){
     struct Node* head=NULL;
-    addFirst(&head,20);
-    addFirst(&head,10);
-    addLast(&head,30);
-    addLast(&head,40);
-
-    printf("LinkedList is as follows : \n");
-    display(&head);
+    addLast(&head,1);
+    addLast(&head,2);
+    addLast(&head,3);
+    addLast(&head,4);
+    display_LL(&head);
 }
