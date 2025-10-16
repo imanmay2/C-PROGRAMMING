dd7f917f28b81b8d01ab0d876ed5260161be6c15
manmay2 <imanmay2@gmail.com>
2025-10-16 21:06:02 +0530

    part-1 linkedList is done

--- src/LL_practise.c
+++ src/LL_practise.c
@@ -55,6 +55,70 @@ void display(struct Node** head){
 }
 
 
+void removeFirst(struct Node** head){
+    struct Node* temp=*head;
+    if(*head==NULL){
+        printf("Linked List is Empty.");
+        return;
+    }
+
+    *head=(*head)->next;
+    printf("Removed first element");
+    return;
+}
+
+
+void removeLast(struct Node** head){
+    struct Node* temp=*head;
+    if(*head==NULL){
+        return;
+    }
+    while(temp->next->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=NULL;
+    printf("Removed the Last Element.");
+}
+
+void addMiddle(struct Node** head,int value,int idx){
+    struct Node* newNode=createNode(value);
+
+    if(*head==NULL){
+        *head=newNode;
+        return;
+    }
+
+    int i=0;
+    struct Node* temp=*head;
+    while(i<idx-1){
+        temp=temp->next;
+        i++;
+    }
+    newNode->next=temp->next;
+    temp->next=newNode;
+    printf("Successfully added in the middle.\n");
+    return;
+}
+
+void removeIndex(struct Node** head,int idx){
+    if(*head==NULL){
+        printf("List is empty.\n");
+        return;
+    }
+
+    struct Node* temp=*head;
+    int i=0;
+    while(i<idx-1){
+        temp=temp->next;
+        i++;
+    }
+
+    temp->next=temp->next->next;
+    printf("Desired element removed.");
+    return;
+}
+
+
 int main(){
     struct Node* head=NULL;
     addFirst(&head,1);
@@ -62,5 +126,14 @@ int main(){
     addLast(&head,3);
     addLast(&head,4);
 
+    display(&head);
+
+    removeFirst(&head);
+    display(&head);
+
+    removeLast(&head);
+    display(&head);
+
+    removeIndex(&head,2);
     display(&head);
 }
